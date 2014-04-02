0c077dd0cbefac2f7b222daf305e39cf5a1d8d70
mtermine <mtermine@gmail.com>
2014-04-02 01:22:32 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TablesorterTest::Application.config.secret_key_base = 'c5f13d2a107c3761b6381c4e6146aa3ce2ee352195c61334e9f400ab615270a5b3fd2e01d02aad3727424c6bcce951b10dcc7e5783a4060dfbe2f8d7bc97bf7f'
